27c1725c3cf4fcfbad79b132ee9e688f0060dbb0
srimathi-thiru <suhisri13@gmail.com>
2024-09-02 09:35:53 +0530

    Add files via upload

---
+++ Day1/Day1Ce1.js
@@ -0,0 +1,16 @@
+import React from 'react';
+function msg()
+{
+  alert("Message to the java developer");
+  console.log("hi");
+}
+function Cw1() {
+  return (
+    <div className="App">
+    <button onClick={msg}>CW1</button>
+    <br></br>
+    </div>
+  );
+};
+
+export default Cw1;
---
+++ Day1/Day1Ce2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function Cw2()
+{
+    return(
+        <div>
+        <h1>Hello Students</h1>,
+        <h2>Welcome to React</h2>
+        </div>
+    )
+}
+export default Cw2;
